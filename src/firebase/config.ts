7775d7aa1baa8e9327d3181115cbe05d650312d7
VALKY SOL <shulysoldaviniiii@gmail.com>
2026-01-05 22:58:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7268991925-944e9",
+  "appId": "1:896545745284:web:c9a7bb2829acefd3c195d1",
+  "apiKey": "AIzaSyBsTW8rThrQpbpXo_vmtoe_ZHFGyfqnIsE",
+  "authDomain": "studio-7268991925-944e9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "896545745284"
+};
