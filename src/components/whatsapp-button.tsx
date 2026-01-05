d9f063817c868a716876c58ee7622763f751e221
VALKY SOL <shulysoldaviniiii@gmail.com>
2026-01-05 22:44:53 +0000

    Try fixing this error: `Console Error: Invalid DOM property `enable-back

--- src/components/whatsapp-button.tsx
+++ src/components/whatsapp-button.tsx
@@ -11,7 +11,7 @@ const WhatsAppIcon = () => (
     version="1.1" 
     x="0px" 
     y="0px" 
-    enable-background="new 0 0 24 24" 
+    enableBackground="new 0 0 24 24" 
     xmlSpace="preserve"
     fill="currentColor"
     >
